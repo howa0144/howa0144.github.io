d30f9794522850b410ac3254a2e49ce581d7dc94
howa0144 <38300485+howa0144@users.noreply.github.com>
2018-04-24 21:57:54 -0400

    first upload attempt

---
+++ final/scripts/validation.js
@@ -0,0 +1,107 @@
+window.onload = init;
+
+function init(){
+
+	$('#btn-submit').on('click', function(event){
+	
+		event.preventDefault();
+
+		required_check();
+	});
+}
+
+function required_check() {
+
+	
+	var required_inputs = $('.required input, .required textarea');
+
+	$.each(required_inputs, function(input_index, req_input) {
+		
+
+		var required_container = $(req_input).closest('.input_container');
+		var input_label = $(required_container).find('.label-text').text();
+		var feedback = $(required_container).find('.feedback');
+		var input_val = $(req_input).val().trim();
+
+		
+		console.log("Label:"+input_label);
+		console.log("Value:"+input_val);
+	
+		$(feedback).fadeOut(0);
+	
+		if(input_val=='') {
+
+			console.log('empty value')
+
+			$(required_container).addClass('has-error');
+			$(required_container).removeClass('has-success');
+
+
+	
+			$(feedback).text();
+
+		} else {
+			console.log('entered value');
+
+			$(required_container).addClass('has-success');
+			$(required_container).removeClass('has-error');
+
+			$(feedback).text();
+		}
+
+		
+		$(feedback).fadeIn(300);
+
+
+	});
+
+	
+	var required_checkbox_containers = $('.required-checkbox');
+
+	
+	$.each(required_checkbox_containers, function(req_check_container_index, req_check_container){
+
+		
+		var required_checkboxes = $(req_check_container).find('input[type=checkbox]');
+		
+		var checkbox_feedback = $(req_check_container).find('.feedback');
+		
+		var checkbox_label = $(req_check_container).find('.label-text').text();
+		
+		var check_counter = 0;
+
+		
+		$(checkbox_feedback).fadeOut(0);
+
+		
+		$.each(required_checkboxes, function(req_check_index, req_check){
+			
+			console.log($(req_check).prop('checked'));
+
+			
+			if($(req_check).prop('checked')) {
+				
+				check_counter++;
+			}
+
+		});
+
+		
+		if(check_counter>0) {
+			
+			$(req_check_container).addClass('has-success');
+			$(req_check_container).removeClass('has-error');
+			$(checkbox_feedback).text("");
+
+		} else {
+			
+			$(req_check_container).removeClass('has-success');
+			$(req_check_container).addClass('has-error');
+			$(checkbox_feedback).text('Please select an option');
+
+		}
+
+		
+		$(checkbox_feedback).fadeIn(300);
+	});
+}
